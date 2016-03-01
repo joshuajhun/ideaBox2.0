a967e1e630c96d043cc8b87312e1990efb600bdc
Matt Stjernholm <thew88@gmail.com>
2016-03-01 15:51:26 -0700

    implement fix to home page js-error

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -24,10 +24,12 @@ function renderPost(newIdea){
 }
 
 function truncateBody(string){
+  if (string){
     if (string.length > 100) {
         return $.trim(string).substring(0, 100).split(" ").slice(0, -1).join(" ") + "...";
     } else {
         return string;
     }
+  }
 
 }
