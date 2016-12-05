3f7a4028fae4064750659bc522fe276f88bd1263
joshuajhun <joshuajhun@gmail.com>
2016-12-05 12:46:43 -0700

    null session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_filter :allow_ajax_request
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   def allow_ajax_request
     headers['Access-Control-Allow-Origin'] = '*'
